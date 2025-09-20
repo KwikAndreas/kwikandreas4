f0f94ec2c8ba4d1df30abf4a9555974a46e5872e
KwikAndreas <138807093+KwikAndreas@users.noreply.github.com>
2025-09-21 00:03:05 +0700

    fix seo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -136,7 +136,7 @@ export default function RootLayout({
     <html lang="en" suppressHydrationWarning className={inter.variable}>
       <head>
         <link rel="icon" href="/favicon.ico" sizes="32x32" />
-        <link rel="icon" href="/icon.svg" type="image/svg+xml" />
+        <link rel="icon" href="/bitgrow.svg" type="image/svg+xml" />
         <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
         <link rel="manifest" href="/manifest.json" />
         <meta name="theme-color" content="#0070f3" />
